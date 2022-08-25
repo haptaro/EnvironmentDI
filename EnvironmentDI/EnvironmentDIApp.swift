1573af1a8761ee736cebfd707c58368d2ef7599c
Kotaro Fukuo <haptaro.dev@gmail.com>
2022-08-25 13:17:58 -0700

    Initial Commit

---
+++ EnvironmentDI/EnvironmentDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentDIApp.swift
+//  EnvironmentDI
+//
+//  Created by Kotaro Fukuo on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
